8ce95907ba3072c4b324afa20c700d5825dd17b7
ask4fusora <thedev@ask4fusora.app>
2026-05-22 23:35:03 +0700

    style(lsp): adjust indentation in vtsls configuration
    Standardize indentation from two spaces to four spaces in
    `lsp/vtsls.lua`.

--- lsp/vtsls.lua
+++ lsp/vtsls.lua
@@ -25,17 +25,17 @@
 
 ---@type vim.lsp.Config
 return {
-  cmd = { 'vtsls', '--stdio' },
-  init_options = {
-    hostInfo = 'neovim',
-  },
-  filetypes = {
-    'javascript',
-    'javascriptreact',
-    'javascript.jsx',
-    'typescript',
-    'typescriptreact',
-    'typescript.tsx',
-  },
-  root_markers = { 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lockb', 'bun.lock', '.git' }
+    cmd = { 'vtsls', '--stdio' },
+    init_options = {
+        hostInfo = 'neovim',
+    },
+    filetypes = {
+        'javascript',
+        'javascriptreact',
+        'javascript.jsx',
+        'typescript',
+        'typescriptreact',
+        'typescript.tsx',
+    },
+    root_markers = { 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lockb', 'bun.lock', '.git' }
 }
