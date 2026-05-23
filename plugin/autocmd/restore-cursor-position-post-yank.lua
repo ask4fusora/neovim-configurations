8f3dfdd2a091bfdf26d43ca7e252ba77d0b45c2b
ask4fusora <thedev@ask4fusora.app>
2026-05-24 06:47:21 +0700

    feat(autocmd): add `RestoreCursorPositionPostYank`

---
+++ plugin/autocmd/restore-cursor-position-post-yank.lua
@@ -0,0 +1,17 @@
+local cursor_position = { 0, 0, 0, 0 }
+
+local group = vim.api.nvim_create_augroup("RestoreCursorPositionPostYank", { clear = true })
+
+vim.api.nvim_create_autocmd({ "VimEnter", "CursorMoved" }, {
+    group = group,
+    callback = function() cursor_position = vim.fn.getpos(".") end
+})
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+    group = group,
+    callback = function()
+        if vim.v.event.operator == "y" then
+            vim.fn.setpos(".", cursor_position)
+        end
+    end
+})
