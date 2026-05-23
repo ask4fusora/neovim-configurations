1d47cc26d254d13a9bebde4342e0df374a831989
ask4fusora <thedev@ask4fusora.app>
2026-05-24 06:46:27 +0700

    refactor: remove unnecessary library setup

--- plugin/autocmd/highlight-on-yank.lua
+++ plugin/autocmd/highlight-on-yank.lua
@@ -1,1 +1,13 @@
-require('autocmd.event.highlight-on-yank').setup(100)
+local TIMEOUT_MS = 100
+
+local group = vim.api.nvim_create_augroup(
+    "HighlightOnYank",
+    { clear = true }
+)
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+    group = group,
+    callback = function()
+        vim.hl.on_yank({ higroup = "Visual", timeout = TIMEOUT_MS })
+    end
+})
