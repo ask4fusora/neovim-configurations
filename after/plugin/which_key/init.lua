e255d0a5174e589f3fc21c8f867050bc799fc149
ask4fusora <thedev@ask4fusora.app>
2026-05-24 15:57:33 +0700

    feat(which_key): configure `which_key` and small triggers

---
+++ after/plugin/which_key/init.lua
@@ -0,0 +1,35 @@
+local miniclue = require('mini.clue')
+
+miniclue.setup({
+    window = {
+        delay = 200,
+        config = {
+            width = 'auto'
+        }
+    },
+
+    triggers = {
+        { mode = 'n', keys = '<Leader>' },
+        { mode = 'v', keys = '<Leader>' },
+        { mode = 'v', keys = 'i' },
+        { mode = 'v', keys = 'a' },
+        { mode = 'n', keys = 'g' },
+        { mode = 'v', keys = 'g' },
+        { mode = 'n', keys = '[' },
+        { mode = 'n', keys = ']' },
+        { mode = 'n', keys = '<C-G>' },
+        { mode = 'v', keys = '<C-G>' },
+        { mode = 'n', keys = '<C-W>' },
+        { mode = 'i', keys = '<C-X>' },
+    },
+
+    clues = {
+        miniclue.gen_clues.builtin_completion(),
+        miniclue.gen_clues.g(),
+        miniclue.gen_clues.marks(),
+        miniclue.gen_clues.registers(),
+        miniclue.gen_clues.square_brackets(),
+        miniclue.gen_clues.windows({ submode_resize = true }),
+        miniclue.gen_clues.z()
+    }
+})
--- plugin/package/add.lua
+++ plugin/package/add.lua
@@ -6,4 +6,5 @@ vim.pack.add({
     { src = 'https://github.com/chomosuke/typst-preview.nvim',       version = '1.*' },
     { src = 'https://github.com/nvim-mini/mini.notify',              version = 'stable' },
     { src = 'https://github.com/nvim-mini/mini.completion',          version = 'stable' },
+    { src = 'https://github.com/nvim-mini/mini.clue',                version = 'stable' },
 })
