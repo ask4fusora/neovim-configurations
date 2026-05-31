a66d40b8e0e7cd2eae83d3090576df8146811991
ask4fusora <thedev@ask4fusora.app>
2026-06-01 00:45:09 +0700

    chore: clean up redundant log

--- plugin/opt/clipboard.lua
+++ plugin/opt/clipboard.lua
@@ -1,7 +1,6 @@
 vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
     once = true,
     callback = function()
-        vim.notify('clipboard', vim.log.levels.ERROR)
         vim.o.clipboard = "unnamedplus"
     end,
 })
