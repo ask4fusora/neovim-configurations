8359a4e330777fcce47f61fb1ba2fdf8b2a233db
ask4fusora <theswe@ask4fusora.dev>
2026-06-29 00:09:01 +0700

    Remove redundant `initialization_options` for `tinymist`

--- lsp/tinymist.lua
+++ lsp/tinymist.lua
@@ -1,9 +1,6 @@
 ---@type vim.lsp.Config
 return {
   cmd = { "tinymist", "lsp" },
-  init_options = {
-    formatterMode = "typstyle"
-  },
   filetypes = { "typst" },
   root_markers = { ".git" }
 }
