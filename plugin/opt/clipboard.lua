ac4c97c52542bec07f61c6d21cd841a747b6a6d4
ask4fusora <theswe@ask4fusora.dev>
2026-07-01 01:21:24 +0700

    yeah yeah

--- plugin/opt/clipboard.lua
+++ plugin/opt/clipboard.lua
@@ -1,4 +1,4 @@
-vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
     once = true,
     callback = function()
         vim.o.clipboard = "unnamedplus"
