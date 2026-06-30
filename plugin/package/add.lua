f96575c5f4fe80c7ef0c7292106eaf36239e4464
ask4fusora <theswe@ask4fusora.dev>
2026-06-30 18:56:06 +0700

    Format package plugin spec consistently

--- plugin/package/add.lua
+++ plugin/package/add.lua
@@ -1,13 +1,28 @@
 vim.pack.add({
     { src = "https://github.com/nvim-lua/plenary.nvim" },
-    { src = 'https://github.com/nickkadutskyi/jb.nvim' },
-    { src = 'https://github.com/MunifTanjim/nui.nvim' },
-    { src = 'https://github.com/OlegGulevskyy/better-ts-errors.nvim' },
-    { src = 'https://github.com/arborist-ts/arborist.nvim' },
-    { src = 'https://github.com/chomosuke/typst-preview.nvim',       version = vim.version.range('1.*') },
-    { src = 'https://github.com/nvim-mini/mini.notify',              version = 'stable' },
-    { src = 'https://github.com/nvim-mini/mini.completion',          version = 'stable' },
-    { src = 'https://github.com/nvim-mini/mini.clue',                version = 'stable' },
-    { src = 'https://github.com/nvim-mini/mini.files',               version = 'stable' },
-    { src = 'https://github.com/dmtrKovalenko/fff' }
+    { src = "https://github.com/nickkadutskyi/jb.nvim" },
+    { src = "https://github.com/MunifTanjim/nui.nvim" },
+    { src = "https://github.com/OlegGulevskyy/better-ts-errors.nvim" },
+    { src = "https://github.com/arborist-ts/arborist.nvim" },
+    {
+        src = "https://github.com/chomosuke/typst-preview.nvim",
+        version = vim.version.range("1.*"),
+    },
+    {
+        src = "https://github.com/nvim-mini/mini.notify",
+        version = "stable",
+    },
+    {
+        src = "https://github.com/nvim-mini/mini.completion",
+        version = "stable",
+    },
+    {
+        src = "https://github.com/nvim-mini/mini.clue",
+        version = "stable",
+    },
+    {
+        src = "https://github.com/nvim-mini/mini.files",
+        version = "stable",
+    },
+    { src = "https://github.com/dmtrKovalenko/fff" },
 })
