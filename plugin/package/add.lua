754dcbb3209143e0b5f771d918c606b8947072b3
ask4fusora <theswe@ask4fusora.dev>
2026-06-30 22:26:25 +0700

    feat: remove inactive plugins

--- plugin/package/add.lua
+++ plugin/package/add.lua
@@ -1,8 +1,5 @@
 vim.pack.add({
-    { src = "https://github.com/nvim-lua/plenary.nvim" },
     { src = "https://github.com/nickkadutskyi/jb.nvim" },
-    { src = "https://github.com/MunifTanjim/nui.nvim" },
-    { src = "https://github.com/OlegGulevskyy/better-ts-errors.nvim" },
     { src = "https://github.com/arborist-ts/arborist.nvim" },
     {
         src = "https://github.com/chomosuke/typst-preview.nvim",
