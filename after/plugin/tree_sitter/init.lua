cd762d0644997ab0087df442b301e6cf918a8127
ask4fusora <theswe@ask4fusora.dev>
2026-07-01 13:58:29 +0700

    feat: lazy load tree-sitter on `FileType`

--- after/plugin/tree_sitter/init.lua
+++ after/plugin/tree_sitter/init.lua
@@ -1,9 +1,9 @@
-vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+vim.api.nvim_create_autocmd("FileType", {
     once = true,
     callback = function()
         require("arborist").setup({
             update_cadence = "weekly",
-            install_popular = false,
+            install_popular = true,
             ensure_installed = {
                 "markdown",
                 "markdown_inline",
@@ -12,9 +12,15 @@ vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
                 "astro",
                 "zig",
                 "rust",
+                "c",
                 "cpp",
+                "php",
+                "ruby",
+                "odin",
                 "sql",
                 "lua",
+                "luap",
+                "luadoc",
                 "luau",
                 "typescript",
                 "tsx",
