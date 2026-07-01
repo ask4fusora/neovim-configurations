0a1371ae2225a6e5981d36184945edbf3c7c5242
ask4fusora <theswe@ask4fusora.dev>
2026-07-01 13:17:13 +0700

    feat: set max side-by-side windows to 3

--- after/plugin/file_explorer/init.lua
+++ after/plugin/file_explorer/init.lua
@@ -2,7 +2,11 @@ local keymap = require("neovim.keymap")
 
 keymap.lazy_load(
     function()
-        require("mini.files").setup({})
+        require("mini.files").setup({
+            windows = {
+                max_number = 3,
+            },
+        })
         require("neovim.g").insert_to_list(
             "nominicompletion_filetypes",
             "minifiles"
