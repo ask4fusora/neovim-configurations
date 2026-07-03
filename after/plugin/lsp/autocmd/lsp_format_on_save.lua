3b37cf3b409f83142e4cc507995ac1ca732371fb
ask4fusora <theswe@ask4fusora.dev>
2026-07-03 21:17:25 +0700

    Remove disabled LSP format-on-save autocmd

--- after/plugin/lsp/autocmd/lsp_format_on_save.lua
+++
@@ -1,8 +0,0 @@
-local group = vim.api.nvim_create_augroup("LspFormatOnSave", { clear = true })
-
--- vim.api.nvim_create_autocmd("BufWritePre", {
---     group = group,
---     callback = function()
---         vim.lsp.buf.format()
---     end,
--- })
