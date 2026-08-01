1cf486ee0398ecf9fca0065c87ba76ff0ecbb75a
ask4fusora <theswe@ask4fusora.dev>
2026-08-01 17:30:32 +0700

    Implemented asynchronous-safe lsp document highlight handler

---
+++ after/plugin/lsp/document_highlight/init.lua
@@ -0,0 +1,42 @@
+local DEBOUNCE_DELAY_MS = 100
+
+local debounce_fn = require("neovim.util").debounce_fn
+
+---@type integer?
+local under_cursor_request_id = nil
+
+local function document_highlight()
+    local clients = vim.lsp.get_clients({ bufnr = 0 })
+
+    for _, client in ipairs(clients) do
+        local _, request_id = client:request(
+            "textDocument/documentHighlight",
+            vim.lsp.util.make_position_params(nil, client.offset_encoding),
+            function(err, result, context, config)
+                if context.request_id ~= under_cursor_request_id then
+                    return
+                end
+
+                if result then
+                    vim.lsp.handlers["textDocument/documentHighlight"](
+                        err,
+                        result,
+                        context,
+                        config
+                    )
+                end
+            end
+        )
+
+        -- Update `under_cursor_request_id` to the latest request ID.
+        under_cursor_request_id = request_id
+    end
+end
+
+vim.api.nvim_create_autocmd("CursorMoved", {
+    group = vim.api.nvim_create_augroup("LspDocumentHighlight", { clear = true }),
+    callback = debounce_fn(function()
+        vim.lsp.buf.clear_references()
+        document_highlight()
+    end, DEBOUNCE_DELAY_MS)
+})
