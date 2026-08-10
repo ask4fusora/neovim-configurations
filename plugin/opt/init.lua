d26a348b750ae809076dba9ba03fde721c374408
ask4fusora <theswe@ask4fusora.dev>
2026-08-11 02:27:15 +0700

    Turn on cursor line

--- plugin/opt/init.lua
+++ plugin/opt/init.lua
@@ -13,6 +13,7 @@ vim.o.relativenumber = true
 vim.o.numberwidth = 5
 vim.o.statuscolumn = " %s%=%{v:relnum ? v:relnum : v:lnum} "
 vim.o.signcolumn = "yes:1"
+vim.o.cursorline = true
 
 -- Indentation.
 
