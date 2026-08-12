5c7bfaf4c05e6d0d843ac7849481fd8ec461ace1
ask4fusora <theswe@ask4fusora.dev>
2026-08-12 15:29:16 +0700

    Lazy load notification module

--- after/plugin/notification/init.lua
+++ after/plugin/notification/init.lua
@@ -1,15 +1,22 @@
-require("mini.notify").setup({
-    window = { max_width_share = 0.618 },
-    content = {
-        format = function(notification)
-            local time_string =
-                vim.fn.strftime("%H:%M:%S", math.floor(notification.ts_update))
+vim.api.nvim_create_autocmd("SafeState", {
+    once = true,
+    callback = function()
+        require("mini.notify").setup({
+            window = { max_width_share = 0.618 },
+            content = {
+                format = function(notification)
+                    local time_string = vim.fn.strftime(
+                        "%H:%M:%S",
+                        math.floor(notification.ts_update)
+                    )
 
-            return (" %s | %s | %s "):format(
-                time_string,
-                notification.level,
-                notification.msg
-            )
-        end,
-    },
+                    return (" %s | %s | %s "):format(
+                        time_string,
+                        notification.level,
+                        notification.msg
+                    )
+                end,
+            },
+        })
+    end,
 })
