51ca881865cfdb250e578f673e6c797d9865783f
ask4fusora <theswe@ask4fusora.dev>
2026-08-17 16:51:24 +0700

    Load filetype setup on startup

--- init.lua
+++ init.lua
@@ -4,7 +4,12 @@ require("vim._core.ui2").enable()
 ---@type fsr.initializer.Registration[]
 local registry = {
     {
-        module_names = { "opt.setup", "plugin.setup", "appearance.setup" },
+        module_names = {
+            "opt.setup",
+            "plugin.setup",
+            "appearance.setup",
+            "filetype.setup",
+        },
     },
 }
 
