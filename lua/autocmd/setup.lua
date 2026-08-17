5588b106057c9046b08b97e6f7cce5425bafb410
ask4fusora <theswe@ask4fusora.dev>
2026-08-17 17:00:32 +0700

    Rename quickfix autocmd module

--- lua/autocmd/setup.lua
+++ lua/autocmd/setup.lua
@@ -1,4 +1,4 @@
 require("initializer").require_modules({
     "autocmd.command.highlight_on_yank",
-    "autocmd.command.open_quickfix_list_post_grep",
+    "autocmd.command.open_quick_fix_list_post_grep",
 })
