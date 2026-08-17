6597ef710005b2945dd596e8184f7abfad7e6b83
ask4fusora <theswe@ask4fusora.dev>
2026-08-17 17:54:21 +0700

    Simplify cursor highlight setup

--- lua/appearance/setup.lua
+++ lua/appearance/setup.lua
@@ -11,11 +11,6 @@ if not success then
     return
 end
 
-local function update_cursor_hl_group()
-    local hl_group = vim.api.nvim_get_hl(0, { name = "Custom_Statusbar" })
-    vim.api.nvim_set_hl(0, "Cursor", { force = true, bg = hl_group.fg })
-end
-
 jb.setup({
     transparent = true,
     snacks = { explorer = { enabled = false } },
@@ -23,23 +18,7 @@ jb.setup({
 })
 
 vim.cmd.colorscheme("jb")
-update_cursor_hl_group()
 
-local color_scheme_group = vim.api.nvim_create_augroup(
-    "fsr.UpdateCursorHlGroupOnColorScheme",
-    { clear = true }
-)
-vim.api.nvim_create_autocmd(
-    "ColorScheme",
-    { group = color_scheme_group, callback = update_cursor_hl_group }
-)
-
-local option_set_group = vim.api.nvim_create_augroup(
-    "fsr.UpdateCursorHlGroupOnOptionSet",
-    { clear = true }
-)
-vim.api.nvim_create_autocmd("OptionSet", {
-    pattern = "background",
-    group = option_set_group,
-    callback = update_cursor_hl_group,
-})
+-- Update `Cursor` highlight group.
+local hl_group = vim.api.nvim_get_hl(0, { name = "Custom_Statusbar" })
+vim.api.nvim_set_hl(0, "Cursor", { force = true, bg = hl_group.fg })
