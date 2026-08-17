a9f4dab556e7f3070e8c4c2f02744ea3bc010868
ask4fusora <theswe@ask4fusora.dev>
2026-08-17 15:47:10 +0700

    Add initializer type annotations

---
+++ lua/initializer/types.lua
@@ -0,0 +1,7 @@
+---@class fsr.initializer.Module
+---@field setup fun()
+
+---@class fsr.initializer.Registration
+---@field event string
+---@field pattern string|string[]?
+---@field modules fsr.initializer.Module[]
